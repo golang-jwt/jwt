6296976a510f178efbcb5fd8401cd7ab10c51d9c
Tom Anderson <tom.anderson@apod.com.au>
2021-11-06 22:32:03 +1030

    Parsing PKCS1 Public Key

--- rsa_utils.go
+++ rsa_utils.go
@@ -75,7 +75,7 @@ func ParseRSAPrivateKeyFromPEMWithPassword(key []byte, password string) (*rsa.Pr
 	return pkey, nil
 }
 
-// ParseRSAPublicKeyFromPEM parses a PEM encoded PKCS1 or PKCS8 public key
+// ParseRSAPublicKeyFromPEM parses a certificate or a PEM encoded PKCS1 or PKIX public key
 func ParseRSAPublicKeyFromPEM(key []byte) (*rsa.PublicKey, error) {
 	var err error
 
@@ -87,9 +87,13 @@ func ParseRSAPublicKeyFromPEM(key []byte) (*rsa.PublicKey, error) {
 
 	// Parse the key
 	var parsedKey interface{}
-	if parsedKey, err = x509.ParsePKCS1PublicKey(block.Bytes); err != nil {
-		if parsedKey, err = x509.ParsePKCS8PublicKey(block.Bytes); err != nil {
-			return nil, err
+	if parsedKey, err = x509.ParsePKIXPublicKey(block.Bytes); err != nil {
+		if cert, err := x509.ParseCertificate(block.Bytes); err == nil {
+			parsedKey = cert.PublicKey
+		} else {
+			if parsedKey, err = x509.ParsePKCS1PublicKey(block.Bytes); err == nil {
+				return nil, err
+			}
 		}
 	}
 
