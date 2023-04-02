62073571dfcbbf9dfa041426814a49a9f26d57e6
zhouyiheng.go <zhouyiheng.go@bytedance.com>
2023-04-02 23:54:36 +0800

    feat: custom json and base64 encoders for Token and Parser
    Co-Authored-By: Christian Banse <oxisto@aybaze.com>

--- token_option.go
+++ token_option.go
@@ -3,3 +3,15 @@ package jwt
 // TokenOption is a reserved type, which provides some forward compatibility,
 // if we ever want to introduce token creation-related options.
 type TokenOption func(*Token)
+
+func WithJSONEncoder(f JSONMarshalFunc) TokenOption {
+	return func(token *Token) {
+		token.jsonEncoder = f
+	}
+}
+
+func WithBase64Encoder(f Base64EncodeFunc) TokenOption {
+	return func(token *Token) {
+		token.base64Encoder = f
+	}
+}
