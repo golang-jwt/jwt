c035977d9e11c351f4c05dfeae193923cbab49ee
Mattt <mattt@me.com>
2025-01-18 06:57:31 -0800

    Update Parse example to use WithValidMethods (#425)

--- hmac_example_test.go
+++ hmac_example_test.go
@@ -49,14 +49,9 @@ func ExampleParse_hmac() {
 	// head of the token to identify which key to use, but the parsed token (head and claims) is provided
 	// to the callback, providing flexibility.
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		// Don't forget to validate the alg is what you expect:
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-		}
-
 		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
 		return hmacSampleSecret, nil
-	})
+	}, jwt.WithValidMethods([]string{jwt.SigningMethodHS256.Alg()}))
 	if err != nil {
 		log.Fatal(err)
 	}
